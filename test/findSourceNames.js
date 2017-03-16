d3f38561b6770739d40e159b930b0ea87afef1bd
jhpoelen <jhpoelen@gmail.com>
2017-03-16 13:54:52 -0700

    ensure to include source names only once

--- test/findSourceNames.js
+++ test/findSourceNames.js
@@ -2,13 +2,26 @@ var globiData = require('../');
 var test = require('tape');
 
 test('find names', function (t) {
-  t.plan(3);
-  globiData.findSourceNames(function(sourceNames) {
-    t.ok(sourceNames.length > 0);
-    var sourceNamesWithName = sourceNames.filter(function(name) { return name.name !== undefined; });
-    t.equal(sourceNames.length, sourceNamesWithName.length);
-    var sourceNamesWithDoi = sourceNames.filter(function(name) { return name.doi !== undefined; });
-    t.ok(sourceNamesWithDoi.length > 0);
-  });
+    t.plan(4);
+    globiData.findSourceNames(function (sourceNames) {
+        t.ok(sourceNames.length > 0);
+        var sourceNamesWithName = sourceNames.filter(function (name) {
+            return name.name !== undefined;
+        });
+        t.equal(sourceNames.length, sourceNamesWithName.length);
+        var sourceNamesWithDoi = sourceNames.filter(function (name) {
+            return name.doi !== undefined;
+        });
+        t.ok(sourceNamesWithDoi.length > 0);
+        var sourceNamesAll = sourceNames.map(function (name) {
+            return name.name;
+        });
+        var sourceNamesUniq = sourceNamesAll.sort().filter(function (name, i) {
+            var of = sourceNamesAll.indexOf(name);
+            return of === i;
+        });
+
+        t.equal(sourceNames.length, sourceNamesUniq.length);
+    });
 
 });
